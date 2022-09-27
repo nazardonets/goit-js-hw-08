365cb3640f586a3acf6416744ca1f524cf13487b
Nazar D <nazardonets@gmail.com>
2022-09-28 00:46:44 +0300

    finished task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainerEl = document.querySelector('.gallery');
+
+const galleryItemTemplate = ({ preview, original, description }) =>
+  `
+		<a class="gallery__item" href="${original}">
+			<img
+				class="gallery__image"
+				src="${preview}"
+				data-source="${original}"
+				alt="${description}"
+			/>
+		</a>
+	`;
+
+const renderUI = () => {
+  const imagesListHtml = galleryItems
+    .map(image => galleryItemTemplate(image))
+    .join('');
+  galleryContainerEl.insertAdjacentHTML('beforeend', imagesListHtml);
+};
+
+renderUI();
+
+let gallery = new SimpleLightbox('.gallery a', {
+  overlayOpacity: 0.9,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
