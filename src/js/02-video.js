14159f193d276fd03805de93a79ada141da92d79
Nazar D <nazardonets@gmail.com>
2022-09-28 23:28:50 +0300

    updated implementation of task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,22 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
 let playbackCurrentSecond;
 
-player.on('timeupdate', data => {
-   playbackCurrentSecond = parseInt(data.seconds);
-   localStorage.setItem('playbackCurrentSecond', `${playbackCurrentSecond}`);
-});
+player.on(
+   'timeupdate',
+   throttle(data => {
+      playbackCurrentSecond = parseInt(data.seconds);
+      localStorage.setItem(
+         'videoplayer-current-time',
+         `${playbackCurrentSecond}`
+      );
+   }, 1000)
+);
 
 player.on('loaded', () => {
-   player.setCurrentTime(localStorage.getItem('playbackCurrentSecond'));
+   player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
 });
