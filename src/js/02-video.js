02bf8f2a7200d3dad83ff748d8b29341d7276fed
Nazar D <nazardonets@gmail.com>
2022-09-28 19:09:29 +0300

    finished task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+let playbackCurrentSecond;
+
+player.on('timeupdate', data => {
+  playbackCurrentSecond = parseInt(data.seconds);
+  localStorage.setItem('playbackCurrentSecond', `${playbackCurrentSecond}`);
+});
+
+player.on('loaded', () => {
+  player.setCurrentTime(localStorage.getItem('playbackCurrentSecond'));
+});
